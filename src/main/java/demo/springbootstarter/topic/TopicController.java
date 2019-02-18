4d33e702a38df6f2e35863df5eb8f8197c7663f8
unknown <sumit.bhojak899@gmail.com>
2019-02-18 09:39:31 +0530

    modified controller

--- src/main/java/demo/springbootstarter/topic/TopicController.java
+++ src/main/java/demo/springbootstarter/topic/TopicController.java
@@ -3,6 +3,8 @@ package demo.springbootstarter.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,28 +18,43 @@ public class TopicController {
 	private TopicService topicService ;
 	
 	@RequestMapping("/topics")
-	public List<Topic> getAllTopics() {
-		return topicService.getAllTopics();
+	public ResponseEntity<List<Topic>> getAllTopics() {
+		List<Topic> topics = topicService.getAllTopics();
+		if(topics.isEmpty()) {
+			return new ResponseEntity<List<Topic>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Topic>>(topics, HttpStatus.OK);
 	}
 	
 	@RequestMapping("/topics/{id}")
-	public Topic getTopic(@PathVariable String id) {
-		return topicService.getTopic(id);
+	public ResponseEntity<Topic> getTopic(@PathVariable String id) {
+		System.out.println("Fetching topic with id" +id);
+		Topic topic = topicService.getTopic(id);
+		if(topic == null) {
+			System.out.println("Topic with id" +id + "not found");
+			return new ResponseEntity<Topic>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Topic>(topic, HttpStatus.OK);
+		
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value="/topics")
-	public void addTopic(@RequestBody Topic topic) {
+	public ResponseEntity<Void> addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+		
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
-	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
-		topicService.updateTopic(id, topic);
+	public ResponseEntity<Void> updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.addTopic(topic);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value="/topics/{id}")
-	public void deleteTopic(@PathVariable String id) {
+	public ResponseEntity<Void> deleteTopic(@PathVariable String id) {
 		 topicService.deleteTopic(id);
+		 return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 	
 }
